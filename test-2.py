0cd732ca857d2e8d82714479bb56590f2ebc531a
Debjyoti Chowdhury <djbabai.debjyoti@gmail.com>
2025-01-02 20:51:08 +0530

    updates

--- test-2.py
+++ test-2.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 
 data_fetcher = DataFetcher(serial_port="/dev/cu.usbserial-0001", baud_rate=115200)
 
-data_fetcher.log_sensor_data(class_label=1, num_samples=10, add_timestamp=False, add_count=True)
+# data_fetcher.log_sensor_data(class_label="hand", num_samples=10, add_timestamp=False, add_count=True)
 
-# while True:
-#     plt.clf()
-#     sensor_data_numpy = data_fetcher.fetch_data(return_as_numpy=True).reshape(8,8)
-#     plt.imshow(sensor_data_numpy)
-#     plt.pause(1)
+while True:
+    plt.clf()
+    sensor_data_numpy = data_fetcher.fetch_data(return_as_numpy=True).reshape(8,8)
+    plt.imshow(sensor_data_numpy)
+    plt.pause(1)
